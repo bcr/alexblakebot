294e84ff1b01eebe9bb5792990a64f99a161de44
Blake Ramsdell <s-blake.ramsdell@lwtech.edu>
2018-11-19 19:23:00 -0800

    Added finding rectangle.

--- classifier.py
+++ classifier.py
@@ -14,4 +14,18 @@ upper_red = np.array([130,255,255])
 
 mask = cv2.inRange(hsv, lower_red, upper_red)
 
-cv2.imwrite('mask.jpg', mask)
+# mask should now have 1 bits where there is red
+
+# Find the contours
+im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+rects = map(lambda contour: cv2.boundingRect(contour), contours)
+
+rects = sorted(rects, reverse=True, key=lambda rectangle1: (rectangle1[2] * rectangle1[3]))
+
+rect = rects[0]
+
+[x,y,w,h] = rect
+cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),5)
+
+cv2.imwrite('mask.jpg', frame)
