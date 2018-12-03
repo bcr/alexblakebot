cb0fc898b294f626a39fa9f529a1bfb7f3d575b9
Blake Ramsdell <s-blake.ramsdell@lwtech.edu>
2018-12-02 18:25:27 -0800

    First cut of range sensor object.

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
+import RPi.GPIO as GPIO
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 
 import classifier
+import range
 
+GPIO.setmode(GPIO.BCM)
 
-# Initialize distance sensor
+trig_pin = 23
+echo_pin = 24
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+try:
+	range_sensor = range.RangeSensor(trig_pin, echo_pin)
 
-# allow the camera to warmup
-time.sleep(0.1)
+	# Initialize distance sensor
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	raw_frame = frame.array
+	camera = PiCamera()
+	camera.resolution = (640, 480)
+	camera.framerate = 32
+	rawCapture = PiRGBArray(camera, size=(640, 480))
 
-	# Identify the target rectangle
-	rect = classifier.target_rect_from_frame(raw_frame)
+	# allow the camera to warmup
+	time.sleep(0.1)
 
-	if rect is None:
-		# Don't see no red
-		print("No red, man        ", end="\r")
-		pass
-	else:
-		vector = (((rect[0] + rect[2]) / 2) - 320) / 320
-		print("Vector", vector, end="\r")
+	frame_number = 1
 
-	# Clear out for next frame
+	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		raw_frame = frame.array
 
-	rawCapture.truncate(0)
+		# Identify the target rectangle
+		rect = classifier.target_rect_from_frame(raw_frame)
 
-# Determine the new vector
-# Determine the new speed
-# Set course for adventure
+		# Verify range to target (one ping only)
+		range = range_sensor.get_reading()
+
+		if rect is None:
+			# Don't see no red
+			print("No red, man        ", end="\r")
+			pass
+		else:
+			vector = (((rect[0] + rect[2]) / 2) - 320) / 320
+			print("frame", frame_number, "vector", vector, "range", range, end="\r")
+
+		# Determine the new vector
+		# Determine the new speed
+		# Set course for adventure
+
+		# Clear out for next frame
+		rawCapture.truncate(0)
+
+		# Next frame
+		frame_number = frame_number + 1
+finally:
+	GPIO.cleanup()
